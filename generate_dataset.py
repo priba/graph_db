02cd628cba64ba6471bd4054a92c5122ab5e8618
AnjanDutta <duttanjan@gmail.com>
2017-06-26 17:28:02 +0200

    Conflict resolved

--- generate_dataset.py
+++ generate_dataset.py
@@ -41,14 +41,9 @@ parser.add_argument('--nodeAdd', help='node std for adding a node in a source ne
 # Distortion Edge
 parser.add_argument('--edgeMaximum', help='maximum number of new edges that can be added', default=8)
 parser.add_argument('--addEdge', help='probability to add new edge', default=0.1)
-<<<<<<< HEAD
+
 parser.add_argument('--rmEdge', help='probability to new edge', default=0.1)
 parser.add_argument('--edgeConnection', help='new edge connected to existing node', default=0.75)
-
-=======
-parser.add_argument('--rmEdge', help='probability to remove an edge', default=0.1)
-parser.add_argument('--edgeConnection', help='probability new edge is connected to an existing node', default=0.75)
->>>>>>> 8e9d650825c2e55037052c0feb1c177246f76217
 
 args = parser.parse_args()
 
